2b3032777a428104456a199700c262fe3ccf7694
Wasyl123456 <waassyl@wp.pl>
2025-04-09 10:57:40 +0200

    wip: Add Menu

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ from utils.controller import get_user_info
 
 def main():
     print(f"Witaj {users[0]["name"]}")
+
+    while True:
+       print("=============MENU===============")
+       print("0 - zakończ pracę programu")
+       print("1 - pokaż co u moich znajomych")
+       print("2 - dodaj znajomych")
+       print("================================")
+       choice:str=input("wybierz opcję menu: ")
+       if choice == "0": break
+       if choice == "1": get_user_info(users[1:])
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
     get_user_info(users[1:])
 
 if __name__ == "__main__":
