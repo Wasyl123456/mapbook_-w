6ad17c4478ae43810b2096de16e88e6c964bd0c2
Wasyl123456 <waassyl@wp.pl>
2025-04-16 16:52:50 +0200

    wip: Add update user functionality

--- notatnik.py
+++ notatnik.py
@@ -1,18 +1,22 @@
 users: list= [
     {"name":"lukasz","location":"opoczno","posts": 89 },
+    {"name":"michal","location":"dębica","posts": 78 },
 ]
+
 
 
 print(users)
 
 
-def remove_user(users_data:list) -> None:
-    users_name=input("Podaj imię znajomego do usunięcia:")
+def update_user(users_data:list) -> None:
+    users_name=input("Podaj imię znajomego do aktualizacji: ")
     for user in users_data:
         if user["name"] == users_name:
-            users.data.remove(user)
+                user["name"]=input("Podaj nowe imie znajomego:")
+                user["location"]=input("Podaj nową miejscowość:")
+                user["posts"]=input(input("Podaj nową liczbę postów:"))
 
-remove_user(users)
+update_user(users)
 print(users)
 
 
--- utils/controller.py
+++ utils/controller.py
@@ -14,3 +14,11 @@ def remove_user(users_data:list) -> None:
     for user in users_data:
         if user["name"] == users_name:
             users.data.remove(user)
+
+            def update_user(users_data: list) -> None:
+                users_name = input("Podaj imię znajomego do aktualizacji: ")
+                for user in users_data:
+                    if user["name"] == users_name:
+                        user["name"] = input("Podaj nowe imie znajomego:")
+                        user["location"] = input("Podaj nową miejscowość:")
+                        user["posts"] = input(input("Podaj nową liczbę postów:"))
