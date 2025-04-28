126b6c69a7bab20d037ec8398a7854779e838e67
Wasyl123456 <waassyl@wp.pl>
2025-04-28 10:14:34 +0200

    chore: Cleaning repository

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-from utils.model import users
-from utils.controller import get_user_info, add_user, remove_user
-
-def main():
-    print(f"Witaj {users[0]["name"]}")
-
-    while True:
-       print("=============MENU===============")
-       print("0 - zakończ pracę programu")
-       print("1 - pokaż co u moich znajomych")
-       print("2 - dodaj znajomych")
-       print("3 - Usuń znajomego")
-       print("================================")
-       choice:str=input("wybierz opcję menu: ")
-       if choice == "0": break
-       if choice == "1": get_user_info(users[1:])
-       if choice == "2": add_user(users)
-       if choice == "3": remove_user(users)
-
-
-
-
-
-if __name__ == "__main__":
-    main()
-
 
 
 
--- notatnik.py
+++ notatnik.py
@@ -1,23 +1,39 @@
-users: list= [
-    {"name":"lukasz","location":"opoczno","posts": 89 },
-    {"name":"michal","location":"dębica","posts": 78 },
+import requests
+from bs4 import BeautifulSoup
+import folium
+
+users: list=[
+    {"name":"lukasz","location":"Warszawa","posts": 89},
+    {"name":"michal","location":"Dębica","posts": 10 },
+    {"name":"weronika","location":"Opole","posts": 20 },
+    {"name":"sabina","location":"Opoczno","posts": 25 },
+
 ]
 
+def get_coordinates(city_name: str) ->list:
+    address_url:str=f"https://pl.wikipedia.org/wiki/{city_name}"
+    response = requests.get(address_url).text
+    response_html = BeautifulSoup(response, "html.parser")
+    longitude:float=float(response_html.select(".longitude")[1].text.replace(",","."))
+    # print(longitude)
+    latitude:float=float(response_html.select(".latitude")[1].text.replace(",","."))
+    # print(latitude)
+    return [latitude,longitude]
 
 
-print(users)
+def get_map(users_data:list)->None:
+mapa= folium.Map([52.3,21.00], zoom_start=6)
+for user in users_data:
+    folium.Marker(
+        location=get_coordinates(city_name=user["location"]),
+        popup=f'{user["name"]}<br/>{user["location"]}<br/> {user["posts"]}<br/v'
+            f'<img src="{user["picture"]}" alt="{user["picture"]}"/>'
+    ).add_to(mapa)
+
+mapa.save("mapa.html")
 
 
-def update_user(users_data:list) -> None:
-    users_name=input("Podaj imię znajomego do aktualizacji: ")
-    for user in users_data:
-        if user["name"] == users_name:
-                user["name"]=input("Podaj nowe imie znajomego:")
-                user["location"]=input("Podaj nową miejscowość:")
-                user["posts"]=input(input("Podaj nową liczbę postów:"))
-
-update_user(users)
-print(users)
+get_map(users)
 
 
 
